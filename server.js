316fa171d3c0aeabc0cf180b915e90c5ff945f7d
vtr <vrigolon@gmail.com>
2020-04-01 16:33:35 +0100

    admin/create page

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
-const server = express()
-const data = require("./data")
+const methodOverride = require('method-override')
+const fs = require('fs')
 
+
+const server = express()
+const data = require("./data.json")
+
+server.use(express.urlencoded({extended: true}))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
+
 
 server.set("view engine", "njk")
 
@@ -15,7 +22,7 @@ nunjucks.configure("views", {
 })
 
 server.get("/", function(req, res) {
-    return res.render("index", { items: data})
+    return res.render("index", { items: data.recipes})
 })
 
 server.get("/about", function(req, res) {
@@ -23,11 +30,11 @@ server.get("/about", function(req, res) {
 })
 
 server.get("/recipes", function(req, res) {
-  return res.render("recipes", { items: data})
+  return res.render("recipes", { items: data.recipes})
 })
 
 server.get("/recipes/:index", function (req, res) {
-  const recipe = data;
+  const recipe = data.recipes;
   const recipeIndex = req.params.index;
   return res.render("recipe", { items: recipe[recipeIndex]})
 
@@ -37,19 +44,49 @@ server.get("/recipes/:index", function (req, res) {
 
 
 server.get("/admin", function(req, res) {
-  return res.render("admin/admin", { items: data})
+  return res.render("admin/admin", { items: data.recipes})
 })
 
 
 server.get("/admin/recipes/:index", function (req, res) {
-  const recipe = data;
+  const recipe = data.recipes;
   const recipeIndex = req.params.index;
   return res.render("admin/show", { items: recipe[recipeIndex]})
 
 })
 
 server.get("/admin/create", function(req, res) {
+
   return res.render("admin/create")
+})
+
+
+server.post("/admin/create", function(req,res) {
+  const keys = Object.keys(req.body)
+  for(key of keys) {
+    if(req.body[key] == "") {
+      return res.send('Please, fill all fields!')
+    }
+  }
+
+  let { image, title, author, ingredients, preparation, information } = req.body
+
+
+  
+  data.recipes.push({
+    image,
+    title,
+    author,
+    ingredients,
+    preparation,
+    information
+  })
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Write file error")
+  
+    return res.redirect("/admin")
+  })
 })
 
 
